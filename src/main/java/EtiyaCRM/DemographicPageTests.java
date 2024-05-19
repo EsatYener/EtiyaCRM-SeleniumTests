04fa407a2aefc550298c679e37da9e91331239b9
Melodi Bayraktar <melodi_bayraktar@hotmail.com>
2024-05-19 12:02:46 +0300

    DemographicPageTests updated

--- src/main/java/EtiyaCRM/DemographicPageTests.java
+++ src/main/java/EtiyaCRM/DemographicPageTests.java
@@ -135,6 +135,62 @@ public class DemographicPageTests {
         driver.findElement(By.cssSelector(".error")).click();
         driver.findElement(By.cssSelector(".ng-touched:nth-child(2)")).sendKeys("12121221212");
     }
+    @Test
+    public void nonRequiredFields() throws InterruptedException {
+        access_demographic_info_screen_AND_required_field();
+        driver.manage().window().setSize(new Dimension(1280, 642));
+        driver.findElement(By.cssSelector(".inputs:nth-child(4) > .ng-valid")).click();
+        driver.findElement(By.cssSelector(".inputs:nth-child(1) > .ng-valid")).click();
+        driver.findElement(By.cssSelector(".inputs:nth-child(3) > .ng-valid")).click();
+        driver.findElement(By.cssSelector(".etiya-demographic")).click();
+    }
+    @Test
+    public void unsuccessfullAddingDemographicInfo() throws InterruptedException{
+        access_demographic_info_screen_AND_required_field();
+        driver.findElement(By.cssSelector(".inputs:nth-child(1) > .ng-invalid")).click();
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Joe");
+        driver.findElement(By.cssSelector(".left-side > .inputs:nth-child(2) > .ng-untouched")).click();
+        driver.findElement(By.cssSelector(".etiya-demographic")).click();
+        driver.findElement(By.cssSelector(".error")).click();
+        driver.findElement(By.cssSelector(".inputs:nth-child(2) > .ng-touched")).sendKeys("Doe");
+        driver.findElement(By.cssSelector(".etiya-demographic")).click();
+        driver.findElement(By.cssSelector(".form-select")).click();
+        driver.findElement(By.cssSelector(".etiya-demographic")).click();
+        driver.findElement(By.cssSelector(".form-select")).click();
+        {
+            WebElement dropdown = driver.findElement(By.cssSelector(".form-select"));
+            dropdown.findElement(By.xpath("//option[. = 'Female']")).click();
+        }
+        driver.findElement(By.cssSelector(".etiya-demographic")).click();
+        driver.findElement(By.cssSelector(".inputs:nth-child(2) > .ng-untouched")).click();
+        driver.findElement(By.cssSelector(".inputs:nth-child(2) > .ng-untouched")).click();
+        driver.findElement(By.cssSelector(".inputs:nth-child(2) > .ng-untouched")).sendKeys("0001-09-04");
+        driver.findElement(By.cssSelector(".inputs:nth-child(2) > .ng-untouched")).sendKeys("0019-09-04");
+        driver.findElement(By.cssSelector(".inputs:nth-child(2) > .ng-untouched")).sendKeys("0199-09-04");
+        driver.findElement(By.cssSelector(".inputs:nth-child(2) > .ng-untouched")).sendKeys("1999-09-04");
+        driver.findElement(By.cssSelector(".etiya-demographic")).click();
+        driver.findElement(By.cssSelector(".ng-invalid:nth-child(2)")).click();
+        {
+            WebElement element = driver.findElement(By.cssSelector(".form"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).clickAndHold().perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector(".etiya-demographic"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).release().perform();
+        }
+        driver.findElement(By.cssSelector(".form")).click();
+        driver.findElement(By.cssSelector(".right-side > .inputs:nth-child(4) > label")).click();
+        driver.findElement(By.cssSelector(".error")).click();
+        driver.findElement(By.cssSelector(".error")).sendKeys("12121");
+        driver.findElement(By.cssSelector(".etiya-demographic")).click();
+        driver.findElement(By.cssSelector(".error")).click();
+        driver.findElement(By.cssSelector(".etiya-demographic")).click();
+
+
+    }
+
 
 
 
