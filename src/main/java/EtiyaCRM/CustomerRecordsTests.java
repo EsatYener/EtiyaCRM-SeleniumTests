2f25e695289b7d08569024133f8f0e6971d286a4
Melodi Bayraktar <melodi_bayraktar@hotmail.com>
2024-05-21 10:10:46 +0300

    CustomerRecordsTests updated

--- src/main/java/EtiyaCRM/CustomerRecordsTests.java
+++ src/main/java/EtiyaCRM/CustomerRecordsTests.java
@@ -1,99 +1,133 @@
 package EtiyaCRM;
 
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.HashMap;
+import java.util.Map;
 
-public class CustomerRecordsTests extends BaseTest{
-    //FR3-Customer Records Tests
+public class CustomerRecordsTests {
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    private JavascriptExecutor js;
+
     @Before
-    public void beforeTest() throws InterruptedException {
-        login("user1", "password1");
+    public void setUp() {
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<>();
+        accessLoginScreen();
+        performLogin("user1", "password1");
     }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    private void accessLoginScreen() {
+        driver.navigate().to("http://localhost:4200/login");
+        driver.manage().window().maximize();
+    }
+
+    private void performLogin(String username, String password) {
+        enterText(By.id("username"), username);
+        enterText(By.id("password"), password);
+        clickElement(By.xpath("//button[contains(.,'Login')]"));
+    }
+
+    private void enterText(By locator, String text) {
+        WebElement element = driver.findElement(locator);
+        element.click();
+        element.sendKeys(text);
+    }
+
+    private void clickElement(By locator) {
+        driver.findElement(locator).click();
+    }
+
     @Test
-    public void record_list_sorting() throws InterruptedException{
-        driver.findElement(By.id("firstName")).click();
-        driver.findElement(By.id("firstName")).sendKeys("e");
-        driver.findElement(By.xpath("//button[contains(.,'Search')]")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//button[contains(.,'Clear')]")).click();
-        driver.findElement(By.id("firstName")).click();
-        driver.findElement(By.id("firstName")).sendKeys("can");
-        driver.findElement(By.id("lastName")).click();
-        driver.findElement(By.id("lastName")).sendKeys("a");
-        driver.findElement(By.xpath("//button[contains(.,'Search')]")).click();
-        Thread.sleep(5000);
+    public void record_list_sorting() throws InterruptedException {
+        searchAndClear("firstName", "m");
+        search("firstName", "m");
+        search("lastName", "b");
     }
 
     @Test
     public void record_list_limitation() throws InterruptedException {
+        search("firstName", "a");
         String[] dropdownOptions = {"pn_id_3_2", "pn_id_3_1", "pn_id_3_0"};
 
-        driver.findElement(By.id("firstName")).click();
-        driver.findElement(By.id("firstName")).sendKeys("a");
-        driver.findElement(By.xpath("//button[contains(.,'Search')]")).click();
-        Thread.sleep(3000);
-
         for (String option : dropdownOptions) {
-            driver.findElement(By.cssSelector(".p-dropdown-trigger-icon")).click();
-            driver.findElement(By.id(option)).click();
+            clickElement(By.cssSelector(".p-dropdown-trigger-icon"));
+            clickElement(By.id(option));
             Thread.sleep(3000);
         }
     }
 
+    @Test
+    public void record_list_pagination() throws InterruptedException {
+        search("firstName", "m");
+        performPagination();
+    }
 
     @Test
-    public void record_list_pagination() throws InterruptedException{
-        driver.findElement(By.id("firstName")).click();
-        driver.findElement(By.id("firstName")).sendKeys("a");
-        driver.findElement(By.xpath("//button[contains(.,'Search')]")).click();
-        Thread.sleep(3000);
-        driver.findElement(By.xpath("//span[contains(.,'10')]")).click();
-        driver.findElement(By.xpath("//li[@id='pn_id_3_1']/span")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector(".p-paginator-next .p-icon")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector(".p-paginator-prev .p-icon")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector(".p-paginator-last .p-icon")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//div[@id='pn_id_1']/p-paginator/div/button")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//div[@id='pn_id_3']/span")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//li[@id='pn_id_3_0']/span")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//button[contains(.,'2')]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//button[contains(.,'3')]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//button[contains(.,'5')]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector(".p-paginator-last .p-icon")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector(".p-paginator-prev .p-icon")).click();
-        Thread.sleep(3000);
-
-        driver.findElement(By.xpath("//div[@id='pn_id_3']/div")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//li[@id='pn_id_3_2']/span")).click();
+    public void clickable_customerID() throws InterruptedException {
+        search("firstName", "m");
+        clickElement(By.xpath("//a[.='9d9b523b-8a6f-42a1-998c-f42f1db2df63']"));
         Thread.sleep(2000);
 
-        driver.findElement(By.cssSelector(".p-dropdown-trigger")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//li[@id='pn_id_3_3']/span")).click();
+    }
+
+    private void search(String field, String value) throws InterruptedException {
+        enterText(By.id(field), value);
+        clickElement(By.xpath("//button[contains(.,'Search')]"));
         Thread.sleep(2000);
     }
 
+    private void searchAndClear(String field, String value) throws InterruptedException {
+        search(field, value);
+        clickElement(By.xpath("//button[contains(.,'Clear')]"));
+    }
 
+    private void performPagination() throws InterruptedException {
+        clickElement(By.xpath("//span[contains(.,'10')]"));
+        clickElement(By.xpath("//li[@id='pn_id_3_1']/span"));
+        navigatePagination();
+        selectDropdownOption("pn_id_3_2");
+        selectDropdownOption("pn_id_3_3");
+    }
 
-    //Clickable Customer ID eklenecek
-    @Test
-    public void clickable_customerID() throws InterruptedException{}
+    private void navigatePagination() throws InterruptedException {
+        By[] paginationSteps = {
+                By.cssSelector(".p-paginator-next .p-icon"),
+                By.cssSelector(".p-paginator-prev .p-icon"),
+                By.cssSelector(".p-paginator-last .p-icon"),
+                By.xpath("//div[@id='pn_id_1']/p-paginator/div/button"),
+                By.xpath("//div[@id='pn_id_3']/span"),
+                By.xpath("//li[@id='pn_id_3_0']/span"),
+                By.xpath("//button[contains(.,'2')]"),
+                By.xpath("//button[contains(.,'3')]"),
+                By.xpath("//button[contains(.,'5')]"),
+                By.cssSelector(".p-paginator-prev .p-icon"),
+                By.cssSelector(".p-paginator-last .p-icon")
+        };
 
+        int waitTime = 2000;
+
+        for (By step : paginationSteps) {
+            clickElement(step);
+            Thread.sleep(waitTime);
+        }
+    }
+
+    private void selectDropdownOption(String optionId) throws InterruptedException {
+        clickElement(By.xpath("//div[@id='pn_id_3']/div"));
+        Thread.sleep(2000);
+        clickElement(By.id(optionId));
+        Thread.sleep(2000);
+    }
 }
-
